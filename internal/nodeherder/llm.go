474cfa4f5402f10ac0cb0e86eff88d0c95afa1f1
mikeathan <mikeathan@gmail.com>
2026-01-09 20:14:45 +0000

    wip

--- internal/nodeherder/llm.go
+++ internal/nodeherder/llm.go
@@ -13,41 +13,61 @@ func (c *LLMDeviceContext) Summary() string {
 	var b strings.Builder
 
 	for _, d := range c.Devices {
-		b.WriteString("- id: ")
+		b.WriteString("- ")
 		b.WriteString(d.ID)
-		b.WriteString("\n")
-
-		b.WriteString("  name: ")
+		b.WriteString(" | ")
 		b.WriteString(d.Name)
-		b.WriteString("\n")
+		b.WriteString(" | exposes: ")
 
-		if len(d.Exposes) > 0 {
-			b.WriteString("  exposes:\n")
-
-			for _, e := range d.Exposes {
-				b.WriteString("    - ")
-				b.WriteString(e.Name)
-
-				if e.Unit != "" {
-					b.WriteString("  [unit: ")
-					b.WriteString(e.Unit)
-					b.WriteString("]")
-				}
-
-				if len(e.Aggregations) > 0 {
-					b.WriteString("  [aggregations: ")
-					b.WriteString(strings.Join(e.Aggregations, ", "))
-					b.WriteString("]")
-				}
-
-				b.WriteString("\n")
+		for i, e := range d.Exposes {
+			if i > 0 {
+				b.WriteString(", ")
 			}
+			b.WriteString(e.Name)
 		}
 
 		b.WriteString("\n")
 	}
 
 	return b.String()
+	// var b strings.Builder
+
+	// for _, d := range c.Devices {
+	// 	b.WriteString("- id: ")
+	// 	b.WriteString(d.ID)
+	// 	b.WriteString("\n")
+
+	// 	b.WriteString("  name: ")
+	// 	b.WriteString(d.Name)
+	// 	b.WriteString("\n")
+
+	// 	if len(d.Exposes) > 0 {
+	// 		b.WriteString("  exposes:\n")
+
+	// 		for _, e := range d.Exposes {
+	// 			b.WriteString("    - ")
+	// 			b.WriteString(e.Name)
+
+	// 			if e.Unit != "" {
+	// 				b.WriteString("  [unit: ")
+	// 				b.WriteString(e.Unit)
+	// 				b.WriteString("]")
+	// 			}
+
+	// 			if len(e.Aggregations) > 0 {
+	// 				b.WriteString("  [aggregations: ")
+	// 				b.WriteString(strings.Join(e.Aggregations, ", "))
+	// 				b.WriteString("]")
+	// 			}
+
+	// 			b.WriteString("\n")
+	// 		}
+	// 	}
+
+	// 	b.WriteString("\n")
+	// }
+
+	// return b.String()
 }
 
 type LLMDevice struct {
